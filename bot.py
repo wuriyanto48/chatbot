777c9daa68042986a61cd41413ad8b24c09479bf
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-02-09 15:25:19 +0700

    add bot and trainer .py

---
+++ bot.py
@@ -0,0 +1,109 @@
+import json
+import textdistance
+import pandas as pd
+import numpy as np
+import random as rd
+from model import stem_text, load_model, DATASET_FILE
+from trainer import THRESHOLD_SIMILARITY
+
+'''
+get bag of words will convert sentence to bag of words using words list
+'''
+def get_bag_of_words(sentence, words, verbose=True):
+
+    # create array of 0 with lenght of words
+    bag = [0 for _ in range(len(words))]
+
+    sentence_words = stem_text(sentence)
+    # sentence_words = clear_stop_words(sentence)
+
+    df = pd.DataFrame(dtype=float, columns=['word', 'score'])
+    pred_word = []
+    scores = []
+    for sw in sentence_words:
+        for i, w in enumerate(words):
+            r = textdistance.hamming.normalized_similarity(sw, w)
+            if r >= THRESHOLD_SIMILARITY:
+                bag[i] = 1
+                pred_word.append(w)
+                scores.append(r)
+
+                if verbose:
+                    print('bag found : {}, similarity: {}'.format(w, r))
+    df['word'] = pred_word
+    df['score'] = scores
+
+    if not df.empty:
+        highest_scored_word_index = np.argmax(df['score'].values)
+        return (np.array(bag), df['word'][highest_scored_word_index], df['score'][highest_scored_word_index])
+
+    return (np.array(bag), '', 0.0)
+
+
+'''
+test classify
+'''
+def test_classify(sentence):
+    model, (words, labels, training_data, output) = load_model()
+    print(type(model))
+    THRESHOLD = 0.25
+    # generate probabilities from the model
+
+    (bags, pred_word, r) = get_bag_of_words(sentence, words)
+    input_data = pd.DataFrame([bags], dtype=float, index=['input'])
+    
+    predicted = model.predict(input_data)[0]
+    results_index = np.argmax(predicted)
+    print(predicted)
+    tag = labels[results_index]
+
+    print(tag)
+    print(training_data)
+    
+    # filter predicted data by threshold
+    # predicted = [[i, p] for i, p in enumerate(predicted) if p > THRESHOLD]
+    
+    # predicted.sort(key=lambda x: x[1], reverse=True)
+
+    # prob_list = []
+    # for r in predicted:
+    #     prob_list.append((labels[r[0]], str(r[1])))
+    
+    # print(prob_list)
+
+'''
+do_answer
+'''
+def do_answer(sentence):
+    model, (words, labels, training_data, output) = load_model()
+
+    (bags, pred_word, r) = get_bag_of_words(sentence, words)
+    input_data = pd.DataFrame([bags], dtype=float, index=['input'])
+
+    results = model.predict(input_data)[0]
+    results_index = np.argmax(results)
+    tag = labels[results_index]
+
+    print(labels)
+    print(tag)
+
+    dataset = {}
+    with open(DATASET_FILE) as file:
+        dataset = json.load(file)
+
+    responses = []
+    for data in dataset["collections"]:
+        if data['tag'] == tag:
+            responses = data['responses']
+    
+    if r > 0.80:
+        choice = rd.choice(responses)
+        return choice
+
+    if r == 0.0:
+        choice = rd.choice(responses)
+        return choice
+
+    choice = rd.choice(responses)   
+    answer = 'Apakah maksud anda "{}"?, {}'.format(pred_word, choice)
+    return answer
--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import numpy as np
 import os
 import re
-import random as rd
 import json
 import nltk
 import string
@@ -23,12 +22,16 @@ VOCAB_PICKLE_DIR = os.path.join(BASE_DIR, 'data/vocab.pickle')
 
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 DATASET_FILE = os.path.join(DATA_DIR, 'dataset.json')
+DATASET_BASE_WORD_BAHASA = os.path.join(DATA_DIR, 'base_word_bahasa.txt')
 
-BATCH_SIZE = 5
-EPOCHS = 1000
-
-WORD_SIZE = 38
-OUTPUT_SIZE = 6
+'''
+load base words bahasa from textfile
+'''
+def load_base_words_bahasa():
+    base_words = []
+    with open(DATASET_BASE_WORD_BAHASA, 'r') as file:
+        base_words = [w.strip() for w in file.readlines()]
+    return base_words
 
 '''
 save tensorflow trained model
@@ -86,191 +89,3 @@ def stem_text(text):
 
     return [stemmer.stem(word.lower()) for word in text]
 
-'''
-get bag of words will convert sentence to bag of words using words list
-'''
-def get_bag_of_words(sentence, words, verbose=True):
-
-    THRESHOLD = 0.65
-
-    # create array of 0 with lenght of words
-    bag = [0 for _ in range(len(words))]
-
-    sentence_words = stem_text(sentence)
-
-    df = pd.DataFrame(dtype=float, columns=['word', 'score'])
-    pred_word = []
-    scores = []
-    for sw in sentence_words:
-        for i, w in enumerate(words):
-            r = textdistance.hamming.normalized_similarity(sw, w)
-            if r >= THRESHOLD:
-                bag[i] = 1
-                pred_word.append(w)
-                scores.append(r)
-
-                if verbose:
-                    print('bag found : {}, similarity: {}'.format(w, r))
-    df['word'] = pred_word
-    df['score'] = scores
-
-    print(df)
-
-    highest_scored_word_index = np.argmax(df['score'].values)
-    return (np.array(bag), df['word'][highest_scored_word_index], df['score'][highest_scored_word_index])
-
-'''
-test model
-'''
-def test_model(sentence):
-    model, (words, labels, training_data, output) = load_model()
-    (bags, pred_word, r) = get_bag_of_words(sentence, words)
-    print(bags)
-    print(words)
-
-'''
-test classify
-'''
-def test_classify(sentence):
-    model, (words, labels, training_data, output) = load_model()
-    print(type(model))
-    THRESHOLD = 0.25
-    # generate probabilities from the model
-
-    (bags, pred_word, r) = get_bag_of_words(sentence, words)
-    input_data = pd.DataFrame([bags], dtype=float, index=['input'])
-    print('--input--')
-    print(input_data.values)
-    
-    predicted = model.predict(input_data.values)[0]
-    results_index = np.argmax(predicted)
-    print(predicted)
-    tag = labels[results_index]
-
-    print(tag)
-    
-    # filter predicted data by threshold
-    # predicted = [[i, p] for i, p in enumerate(predicted) if p > THRESHOLD]
-    
-    # predicted.sort(key=lambda x: x[1], reverse=True)
-
-    # prob_list = []
-    # for r in predicted:
-    #     prob_list.append((labels[r[0]], str(r[1])))
-    
-    # print(prob_list)
-
-'''
-do_answer
-'''
-def do_answer(sentence):
-    model, (words, labels, training_data, output) = load_model()
-
-    (bags, pred_word, r) = get_bag_of_words(sentence, words)
-    input_data = pd.DataFrame([bags], dtype=float, index=['input'])
-
-    results = model.predict(input_data)[0]
-    results_index = np.argmax(results)
-    tag = labels[results_index]
-
-    print(labels)
-    print(tag)
-
-    dataset = {}
-    with open(DATASET_FILE) as file:
-        dataset = json.load(file)
-
-    for data in dataset["collections"]:
-        if data['tag'] == tag:
-            responses = data['responses']
-    
-
-    answer = ''
-    if r > 0.80:
-        choice = rd.choice(responses)
-        answer = choice
-    else:
-        choice = rd.choice(responses)
-        answer = 'Apakah maksud anda "{}"?, {}'.format(pred_word, choice)
-    return answer
-
-def preprocess_dataset():
-
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-
-    dataset = {}
-
-    with open(DATASET_FILE) as file:
-        dataset = json.load(file)
-
-    for intent in dataset['collections']:
-        for pattern in intent['patterns']:
-
-            # case folding
-            pattern = re.sub(r"\d+", "", pattern)
-            pattern = pattern.translate(str.maketrans("","", string.punctuation)).strip()
-
-            # tokenize word
-            word_token = nltk.word_tokenize(pattern)
-            # insert word token to database vacabulary
-            words.extend(word_token)
-
-            docs_x.append(word_token)
-            docs_y.append(intent['tag'])
-
-        if intent['tag'] not in labels:
-            # create unique label/ class
-            labels.append(intent['tag'])
-
-    # words stemming of list pattern
-    words = stem_text(words)
-
-    # create list of unique stemmed words
-    words = sorted(list(set(words)))
-    labels = sorted(labels)
-
-    return (words, labels, docs_x, docs_y)
-
-'''
-train model
-'''
-def train():
-    
-    (words, labels, docs_x, docs_y) = preprocess_dataset()
-
-    # bag of words
-    training_data = []
-    output = []
-
-    out_empty = [0 for _ in range(len(labels))]
-
-    for x, doc in enumerate(docs_x):
-        bag = []
-
-        word_stemmed = stem_text(doc)
-        
-        for w in words:
-            if w in word_stemmed:
-                bag.append(1)
-            else:
-                bag.append(0)
-        
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
-
-        training_data.append(bag)
-        output.append(output_row)
-
-    # convert to numpy array
-    training_data = np.array(training_data)
-    output = np.array(output)
-
-    model = create_model(len(words), len(output[0]))
-    # model = create_model_from_dnn(len(words), len(output[0]), TF_MODEL_DIR, VOCAB_PICKLE_DIR)
-    model.fit(training_data, output, batch_size=BATCH_SIZE , epochs=EPOCHS)
-    
-    save_model(model, (words, labels, training_data, output))
-    # model.save((words, labels, training_data, output))
